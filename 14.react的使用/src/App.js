81c59cf1ebae480abe582f4d7ce95563f5fff388
xuyongchao6585 <xuyongchao6585@ipalfish.com>
2021-07-06 21:13:29 +0800

    feat: react

---
+++ 14.react的使用/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ContextDemo from './src/ContextDemo.js'
+
+function App() {
+  return (
+    <div className="App">
+      <h3>hello react</h3>
+      <ContextDemo></ContextDemo>
+    </div>
+  )
+}
+
+export default App;
